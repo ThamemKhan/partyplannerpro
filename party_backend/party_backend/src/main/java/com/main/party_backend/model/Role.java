d61f1407a08fd2c7e50810845e2d5e55fda2788a
ThamemKhan <98300151+ThamemKhan@users.noreply.github.com>
2024-03-05 15:45:11 +0530

    Delete party_backend/party_backend directory

--- party_backend/party_backend/src/main/java/com/main/party_backend/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.main.party_backend.model;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- party_backend/party_backend/src/main/java/com/main/party_backend/repository/AddonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.main.party_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.main.party_backend.model.AddonModel;
-
-public interface AddonRepository extends JpaRepository<AddonModel,Integer> {
-
-}
